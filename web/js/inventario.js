a65f559061ff1d7cd987ff42f1b57b76b999a5c5
miguel1137 <carvamiguel5@gmail.com>
2026-09-22 22:47:58 -0500

    Creacion de interfaz para ubicacion de la informacion de los productos

--- web/js/inventario.js
+++ web/js/inventario.js
@@ -4,9 +4,17 @@ const productos=document.querySelectorAll(".tarjetaProducto");
 
 productos.forEach(function(tarjetaProducto){
     tarjetaProducto.innerHTML=`
-    <img src="images/louis.jpg" alt="imagen-Producto" width="200" height="150" style="padding-bottom: 10px;margin-left: 0px;margin-right: 0px;margin-top: 0px; border-radius: 8px;">
-    <span class="datosIndividuales">Nombre producto</span>
-    <span class="datosIndividuales">Talla</span>
-    <span class="datosIndividuales">Costo</span>
+    <div>
+        <img src="images/louis.jpg" alt="imagen-Producto" width="200" height="150" style="padding-bottom: 10px;margin-left: 0px;margin-right: 0px;margin-top: 0px; border-radius: 8px;">
+    </div>
+    <div>
+        <span class="datosIndividuales">Nombre producto</span>
+    </div>
+    <div>
+        <span class="datosIndividuales">Talla</span>
+    </div>
+    <div>
+        <span class="datosIndividuales">Costo</span>
+    </div>
     `
 });
